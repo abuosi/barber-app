ec396125627b6338f80b89734fff8d6134a9632f
Adamo Buosi <adamo.buosi@gmail.com>
2020-09-19 09:37:42 -0300

    Evolução - Segundo Commit

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,4 +1,8 @@
 import React, { useState, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import { UserContext } from '../../contexts/UserContext';
 
 import {
     Container,
@@ -12,14 +16,54 @@ import {
 
 import SignInput from '../../components/SignInput';
 
+import Api from '../../Api';
+
 import BarberLogo from '../../assets/barber.svg';
 import EmailIcon from '../../assets/email.svg';
 import LockIcon from '../../assets/lock.svg';
 
 export default () => {
 
+    const { dispatch: userDispatch } = useContext(UserContext);
+
+    const navigation = useNavigation();
+
     const [emailField, setEmailField] = useState('');
     const [passwordField, setPasswordField] = useState('');
+
+    const handleSignClick = async () => {
+        if(emailField != '' && passwordField != '') {
+
+            let json = await Api.signIn(emailField, passwordField);
+
+            if(json.token) {
+                await AsyncStorage.setItem('token', json.token);
+
+                userDispatch({
+                    type: 'setAvatar',
+                    payload:{
+                        avatar: json.data.avatar
+                    }
+                });
+
+                navigation.reset({
+                    routes:[{name:'MainTab'}]
+                });
+            } else {
+                alert('E-mail e/ou senha errados!');
+            }
+
+        } else {
+            alert("Preencha os campos!");
+        }
+    }
+
+    const handleMessageButtonClick = () => {
+        navigation.reset({
+            routes: [{name: 'SignUp'}]
+        });
+    }
+
 
     return ( 
         <Container>
