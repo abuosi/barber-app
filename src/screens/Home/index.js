9247865d037c777e0e82574372e22b630c16b774
Adamo Buosi <adamo.buosi@gmail.com>
2020-09-22 08:21:17 -0300

    Evolução - Terceiro Commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Platform, RefreshControl } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
 
+import Api from '../../Api';
 
 import { 
     Container,
@@ -12,8 +17,12 @@ import {
     SearchButton,
     LocationArea,
     LocationInput,
-    LocationFinder
+    LocationFinder,
+    LoadingIcon,
+    ListArea
 } from './styles';
+
+import BarberItem from '../../components/BarberItem';
 
 import SearchIcon from '../../assets/search.svg';
 import MyLocationIcon from '../../assets/my_location.svg';
@@ -21,7 +30,61 @@ import MyLocationIcon from '../../assets/my_location.svg';
 export default () => {
 
     const navigation = useNavigation();
+
+    const [locationText, setLocationText] = useState('');
+    const [coords, setCoords] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState([]);
+
+    const handleLocationFinder = async () => {
+        setCoords(null);
+        let result = await request(
+            Platform.OS === 'ios' ?
+                PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+                :
+                PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        );
+
+        if(result == 'granted') {
+            setLoading(true);
+            setLocationText('');
+            setList([]);
+
+            Geolocation.getCurrentPosition((info)=>{
+                setCoords(info.coords);
+                getBarbers();
+            });
+
+        }        
+    }
    
+    const getBarbers = async () => {
+        setLoading(true);
+        setList([]);
+
+        let lat = null;
+        let lng = null;
+        if(coords) {
+            lat = coords.latitude;
+            lng = coords.longitude;
+        }
+
+        let res = await Api.getBarbers(lat, lng, locationText);
+        if(res.error == '') {
+            if(res.loc) {
+                setLocationText(res.loc);
+            }
+            setList(res.data);
+        } else {
+            alert("Erro: "+res.error);
+        }
+
+        setLoading(false);
+    }
+
+    useEffect(()=>{
+        getBarbers();
+    }, []);
 
     return (
         <Container>
@@ -34,11 +97,27 @@ export default () => {
                 </HeaderArea>
 
                 <LocationArea>
-                    <LocationInput />
-                    <LocationFinder>
+                    <LocationInput 
+                        placeholder="Onde você está ?"
+                        placeholderTextColor="#FFFFFF"
+                        value={locationText}
+                        onChangeText={t=>setLocationText(t)}
+                    />
+                    <LocationFinder onPress={handleLocationFinder}>
                         <MyLocationIcon width="26" height="26" fill="#FFFFFF" />
                     </LocationFinder>
                 </LocationArea>
+
+                {loading &&
+                    <LoadingIcon size="large" color="#FFFFFF" />
+                }
+
+                <ListArea>
+                    {list.map((item, k)=>(
+                        <BarberItem key={k} data={item} />
+                    ))}
+                </ListArea>
+
 
             </Scroller>
         </Container>
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -45,5 +45,10 @@ export const LocationFinder = styled.TouchableOpacity`
     width: 24px;
     height: 24px;
 `;
-
-export const LoadingIcon = styled.ActivityIndicator``;
+export const LoadingIcon = styled.ActivityIndicator`
+    margin-top: 50px;
+`;
+export const ListArea = styled.View`
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
